b6dc6578b3e5fc67ad3859b29956296a7b9a5752
Rafael Jose Braga Coelho <braga3.coelho3@gmail.com>
2025-10-03 20:34:49 -0300

    Delete Buffer class from Registro.h

--- Registro.h
+++ Registro.h
@@ -4,29 +4,10 @@ using namespace std;
 
 enum Formato { FIXO, DELIMITADO, COMPRIMENTO };
 
-class Buffer{
-    private:
-    vector<char> data;
-    int ponteiro;
-
-    public: 
-    void packFixo(string str,int tamanho);
-    string unpackFixo(int tamanho); 
-    void packDelimitado(string str,char delim);
-    string unpackDelimitado(char delim);
-    void packComprimento(string str);
-    string unpackComprimento();
-    void pack(int valor);
-    int unpackInt();
-    bool read(istream &stream, int tamanho);
-    void write(ostream &stream);
-    void clear();
-};
-
 class Registro {
-    public: 
-    virtual void pack(Buffer buffer, Formato formato) = 0;
-    virtual void unpack(Buffer buffer, Formato formato) = 0;
-    virtual string getChave() = 0;
-    virtual int getTamanhoFixo() = 0;
+public:
+  virtual void pack(Buffer buffer, Formato formato) = 0;
+  virtual void unpack(Buffer buffer, Formato formato) = 0;
+  virtual string getChave() = 0;
+  virtual int getTamanhoFixo() = 0;
 };
