891e58bd8c9ecfb595d25cb10c1cbc3405847bd4
Rafael Jose Braga Coelho <braga3.coelho3@gmail.com>
2025-10-03 20:34:01 -0300

    Create Buffer.h for organization

---
+++ Buffer.h
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Buffer {
+private:
+  vector<char> data;
+  int ponteiro;
+
+public:
+  void packFixo(string str, int tamanho);
+  string unpackFixo(int tamanho);
+  void packDelimitado(string str, char delim);
+  string unpackDelimitado(char delim);
+  void packComprimento(string str);
+  string unpackComprimento();
+  void pack(int valor);
+  int unpackInt();
+  bool read(istream &stream, int tamanho);
+  void write(ostream &stream);
+  void clear();
+};
