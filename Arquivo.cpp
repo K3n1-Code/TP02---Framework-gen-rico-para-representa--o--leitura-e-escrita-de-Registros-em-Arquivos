9dee42e6cbd6fd30a83e209cc672f82daa76b41c
Rafael Jose Braga Coelho <braga3.coelho3@gmail.com>
2025-10-03 21:36:48 -0300

    Implement lerRegistros

--- Arquivo.cpp
+++ Arquivo.cpp
@@ -7,16 +7,35 @@ template <class T> Arquivo<T>::Arquivo(string nomeArquivo, Formato formato) {
   this->nomeArquivo = nomeArquivo;
   this->formato = formato;
 }
+
 template <class T> void Arquivo<T>::adicionarRegistro(T reg) {
 
   Buffer buffer;
   Formato formato;
   ofstream out;
   out.open(this->nomeArquivo);
+  // fazer um try catch para testar a a abertura de arquivos
 
   reg.pack(buffer, formato);
 
   buffer.write(out);
+  out.close();
+  return;
 }
 
-template <class T> vector<T> Arquivo<T>::lerRegistros() { return vector<T>(); }
+template <class T> vector<T> Arquivo<T>::lerRegistros() {
+  vector<T> registros;
+  Buffer buffer;
+  Formato formato;
+  ifstream in;
+  in.open(this->nomeArquivo);
+  while (??) {
+    T reg;
+    buffer.read(&in, 500); // Desconfie dessa implementação
+    reg.pack(buffer, formato);
+    registros.push_back();
+  }
+  in.close();
+
+  return registros;
+}
